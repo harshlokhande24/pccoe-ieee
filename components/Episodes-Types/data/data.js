7a325a8c6f51ed6bbb19266b8fc380ac7735a6ae
harshlokhande24 <harshlokhande.240803@gmail.com>
2023-08-24 17:16:33 +0530

    home page

--- components/Episodes-Types/data/data.js
+++ components/Episodes-Types/data/data.js
@@ -1,22 +1,22 @@
 const data = [
     {
         id: 1,
-        title: "PRAXIS",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        title: "PRAXIS 2023",
+        description: "The highly anticipated annual technical event organized by ITSA PCCOE. With a legacy of excellence, PRAXIS 23 brings together bright minds, cutting-edge technology, and a platform to explore the realms of science and engineering. From hands-on workshops and riveting competitions to inspiring talks by industry experts, this event is a testament to our commitment to fostering technical curiosity and creativity among participants.",
         image_url: "/tech.svg",
         aos: "fade-right"
     },
     {
         id: 2,
-        title: "AAKAR",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        title: "Flutter and Figma - Bootcamp",
+        description: "Flutter and Figma - Bootcamp opportunity brought in the world of app development and UI/UX design through hands-on learning. The participants got an idea of UI/UX and Front end Development along with introduction to Flutter Technologies. Over 350 people participated in the event.",
         image_url: "/business.svg",
         aos: "fade-left"
     },
     {
         id: 3,
-        title: "Game development",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        title: "IEEE Membership Drive",
+        description: "IEEE Membership Drive, orchestrated by the Information Technology Student Association (ITSA) every year opens the doors to a global community of tech enthusiasts, researchers, and professionals. As a member, folks gain access to cutting-edge research, networking events, workshops, and industry insights that will enrich their academic journey and future career.",
         image_url: "/glisa_m3a_ceo.svg",
         aos: "fade-right"
     }
